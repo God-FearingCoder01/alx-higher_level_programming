d7fe7f00414b458611687426b59988f6f98053b1
root <mhlopenkosikhona@gmail.com>
2023-01-04 02:17:54 +0000

    task 0 first attempt

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import random
+
+number = random.randint(-10, 10)
+
+if number:
+	if number > 0:
+		str = "is positive"
+	else
+		str = "is negative"
+else
+	str = "is zero"
+
+
+print(f"{number} {str}\n");
