9da404c87bfcb250e267a713d718ca1b8a6d6618
GFC-01 <mhlopenkosikhona@gmail.com>
2023-01-06 12:38:28 +0000

    corrected string format

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,19 +1,15 @@
 #!/usr/bin/python3
-def get_number_of_arguments(num):
-	num -= 1
-	if num == 1:
-		str = "argument"
-	else:
-		str = "arguments"
-	if num == 0:
-		ch = '.'
-	else:
-		ch = ':'
-	print(f"{num} {str}{ch}")
-	for i in range(1, num+1):
-		print(f"{i}: {sys.argv[i]}")
-	print(f"The number of arguments entered is {num}")
-
 if __name__ == "__main__":
-	import sys
-	get_number_of_arguments(len(sys.argv))
+    from sys import argv
+    argc = len(argv); argc -= 1
+    if argc == 1:
+        strng = "argument"
+    else:
+        strng = "arguments"
+    if argc == 0:
+        ch = "."
+    else:
+        ch = ":"
+    print("{:d} {}{}".format(argc, strng, ch))
+    for i in range(1, argc+1):
+        print("{:d}: {}".format(i, argv[i]))
