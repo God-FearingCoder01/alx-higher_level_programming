8479f33eaae19db6afe14f597819ce267ed48f3b
root <mhlopenkosikhona@gmail.com>
2023-01-06 07:42:47 +0000

    added extecution  permission

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     from sys import argv
     argc = len(argv); sum = 0
-    for i in range(argc):
+    for i in range(1, argc):
         sum += int(argv[i])
     print(sum)
