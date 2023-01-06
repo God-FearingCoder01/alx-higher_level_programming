30039b2b244685c74c23f3ffc5fa409a88b156a6
GFC-01 <mhlopenkosikhona@gmail.com>
2023-01-06 12:46:26 +0000

    removed multi-assignemnt on a single line

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    argc = len(argv); argc -= 1
+    argc = len(argv)
+    argc -= 1
     if argc == 1:
         strng = "argument"
     else:
