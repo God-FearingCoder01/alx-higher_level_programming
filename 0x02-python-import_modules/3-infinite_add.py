cb8c63e1ed32166e3cec1f53f7b095730d48b48e
root <mhlopenkosikhona@gmail.com>
2023-01-06 00:36:48 +0000

    Tt

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+def get_argv_sum(argc):
+	sum = 0
+	for i in range(1, argc):
+		sum += int(sys.argv[i])
+	return (sum)
+
 if __name__ == "__main__":
-    from sys import argv
-    argc = len(argv)
-    sum = 0
-    for i in range(1, argc):
-        sum += int(argv[i])
-    print(sum)
+	import sys
+	print(get_argv_sum(len(sys.argv)))
