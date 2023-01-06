b1f9eb221913ca85686655cb44c69414bb9a0b31
GFC-01 <mhlopenkosikhona@gmail.com>
2023-01-06 23:54:28 +0000

    corrected for signed last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-signed_number = (number * -1) if (number < 0) else (number)
-last_digit = signed_number % 10
-if last_digit:
-    if last_digit > 5:
+unsigned_number = (number * -1) if (number < 0) else (number)
+last_digit = unsigned_number % 10
+unsigned_last_digit = (last_digit * -1) if (number < 0) else (last_digit)
+if unsigned_last_digit:
+    if unsigned_last_digit > 5:
         s = "and is greater than 5"
     else:
         s = "and is less than 6 and not 0"
 else:
     s = "and is 0"
-print("Last digit of {:d} is {:d} {}".format(number, last_digit, s))
+print("Last digit of {:d} is {:d} {}".format(number, unsigned_last_digit, s))
