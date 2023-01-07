bfac51821af91627862fe2efc9bea1b8bd7c9626
GFC-01 <mhlopenkosikhona@gmail.com>
2023-01-07 01:52:03 +0000

    task 9 first attempt

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    number = abs(number)
+    last_digit = number % 10
+    print("{:d}".format(last_digit), end="") 
+    return last_digit
+
+def abs(num):
+    return (num * -1) if num < 0 else num
