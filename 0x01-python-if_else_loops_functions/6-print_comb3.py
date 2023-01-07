7b23de25660f9813a96ea5badb1c3b38b6f86c64
GFC-01 <mhlopenkosikhona@gmail.com>
2023-01-07 00:28:55 +0000

    task 6 first attempt

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+j = 0; i = 0
+while i < 100:
+    j += 1; k = j
+    while k < 10:
+        if i == 8 and k == 9:
+            print("{:d}{:d}".format(i, k))
+        else:
+            print("{:d}{:d}".format(i, k), end=", ")
+        k += 1;
+    i += 1
