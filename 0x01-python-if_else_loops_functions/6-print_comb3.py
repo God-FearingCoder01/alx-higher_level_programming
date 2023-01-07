25b278fd24eb9f3ac49b475d08633a368f02b432
GFC-01 <mhlopenkosikhona@gmail.com>
2023-01-07 00:30:54 +0000

    removed multiple statements on one line

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-j = 0; i = 0
+j = 0
+i = 0
 while i < 100:
-    j += 1; k = j
+    j += 1
+    k = j
     while k < 10:
         if i == 8 and k == 9:
             print("{:d}{:d}".format(i, k))
