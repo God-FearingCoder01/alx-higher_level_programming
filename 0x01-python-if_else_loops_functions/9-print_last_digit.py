08ae9fb2a8e027c267a39a3303cbcdfdfb8d1771
GFC-01 <mhlopenkosikhona@gmail.com>
2023-01-07 02:01:47 +0000

    corrected pycodesty;e errors

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -2,8 +2,9 @@
 def print_last_digit(number):
     number = abs(number)
     last_digit = number % 10
-    print("{:d}".format(last_digit), end="") 
+    print("{:d}".format(last_digit), end="")
     return last_digit
+
 
 def abs(num):
     return (num * -1) if num < 0 else num
