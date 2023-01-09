8c1fd90cc2a3fe2bcd593bcd2f64f31752ea55c6
GFC-01 <mhlopenkosikhona@gmail.com>
2023-01-09 09:42:19 +0000

    task 11 first attempt'

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    my_list_len = len(my_list)
+    idx_is_negative = (idx < 0)
+    idx_out_of_range = (idx >= my_list_len)
+    if  idx_is_negative or  idx_out_of_range:
+        pass
+    else:
+        my_list_copy = my_list
+        my_list_copy = my_list_copy[:idx] + my_list_copy[-(my_list_len - (idx+1)):]
+        my_list[:] = my_list_copy
+    return my_list
