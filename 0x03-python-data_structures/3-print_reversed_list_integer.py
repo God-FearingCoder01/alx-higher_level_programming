5936be9699e91a9d832c2920fab5c5395b93ad6f
GFC-01 <mhlopenkosikhona@gmail.com>
2023-01-09 10:17:46 +0000

    added an if statement

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,6 +2,9 @@
 def print_reversed_list_integer(my_list=[]):
     str = "{:d}"
     my_list_len = len(my_list)
-    for i in range(my_list_len):
-        current_index = my_list_len - (i + 1)
-        print(str.format(my_list[current_index]))
+    if my_list_len:
+        for i in range(my_list_len):
+            current_index = my_list_len - (i + 1)
+            print(str.format(my_list[current_index]))
+    else:
+        print()
