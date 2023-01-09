81045862b11e8a506ac42ad1bb2187aa1215389f
GFC-01 <mhlopenkosikhona@gmail.com>
2023-01-09 21:53:20 +0000

    added an if statement

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     str = "{:d}"
-    for int_num in my_list[::-1]:
-        print(str.format(int_num))
+    if my_list == []:
+        print()
+    else:
+        for int_num in my_list[::-1]:
+            print(str.format(int_num))
