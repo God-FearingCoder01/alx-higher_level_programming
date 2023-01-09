5594ed44b6e12453516b29a6086b910891b7cc2f
GFC-01 <mhlopenkosikhona@gmail.com>
2023-01-09 21:56:41 +0000

    updated changes

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     str = "{:d}"
-    if my_list == []:
-        print()
-    else:
-        for int_num in my_list[::-1]:
-            print(str.format(int_num))
+    for int_num in my_list[::-1]:
+        print(str.format(int_num))
