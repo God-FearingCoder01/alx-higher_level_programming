b1444f6a703ba1cec2f86179606cd2a3e13ae683
GFC-01 <mhlopenkosikhona@gmail.com>
2023-01-09 21:12:18 +0000

    got rid of continue

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     str = "{:d}"
-    matrix_rows = len(matrix)
-    matrix_columns = len(matrix[0])
-    for i in range(matrix_rows):
-        for j in range(matrix_columns):
-            if j == (matrix_columns-1):
-                print(str.format(matrix[i][j]))
-                continue
-            print(str.format(matrix[i][j]), end=" ")
+    if matrix == [[]]:
+        print()
+    else:
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                if j == (len(matrix[i])-1):
+                    print(str.format(matrix[i][j]))
+                else:
+                    print(str.format(matrix[i][j]), end=" ")
