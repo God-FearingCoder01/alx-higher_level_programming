a0b51227be7d838fcd992d343340cb355aa26bd5
GFC-01 <mhlopenkosikhona@gmail.com>
2023-01-09 21:38:57 +0000

    updated small changes

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -3,13 +3,14 @@ def delete_at(my_list=[], idx=0):
     my_list_len = len(my_list)
     idx_is_negative = (idx < 0)
     idx_out_of_range = (idx >= my_list_len)
-    if  idx_is_negative or  idx_out_of_range:
+    if idx_is_negative or idx_out_of_range:
         pass
     else:
+        upperbound = my_list_len - 1
         my_list_copy = my_list
-        if idx == (my_list_len - 1):
+        if idx == upperbound:
             my_list_copy = my_list_copy[:idx]
         else:
-            my_list_copy = my_list_copy[:idx] + my_list_copy[-(my_list_len - (idx+1)):]
+            my_list_copy = my_list_copy[:idx] + my_list_copy[-(upperbound - idx):]
         my_list[:] = my_list_copy
     return my_list
