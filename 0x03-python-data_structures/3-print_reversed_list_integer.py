c1612a3d560ed719b03df1e01bb5393be6213e97
GFC-01 <mhlopenkosikhona@gmail.com>
2023-01-10 15:20:54 +0000

    redone task

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     str = "{:d}"
-    if my_list == None:
-        pass
-    else:
-        for int_num in my_list[::-1]:
-            print(str.format(int_num))
+    if my_list != None:
+        for i in my_list[::-1]:
+            print(str.format(i))
