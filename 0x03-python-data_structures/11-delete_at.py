8099ef8d85b8a742c27f0c1444c6d6f5e3378fad
GFC-01 <mhlopenkosikhona@gmail.com>
2023-01-10 06:18:37 +0000

    reduced length of longest line

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -11,6 +11,7 @@ def delete_at(my_list=[], idx=0):
         if idx == upperbound:
             my_list_copy = my_list_copy[:idx]
         else:
-            my_list_copy = my_list_copy[:idx] + my_list_copy[-(upperbound - idx):]
+            my_list_copy = my_list_copy[:idx] +\
+my_list_copy[-(upperbound - idx):]
         my_list[:] = my_list_copy
     return my_list
