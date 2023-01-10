ae61faffe7c9c3346fdc858c42bfa2d335d282d6
GFC-01 <mhlopenkosikhona@gmail.com>
2023-01-10 16:20:52 +0000

    corrected pycodestyle error E231

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     operator = argv[2]
     b = int(argv[3])
     if operator == "+":
-        print("{:d} + {:d} = {:d}".format(a, b, add(a,b)))
+        print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
     elif operator == "-":
         print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
     elif operator == "*":
