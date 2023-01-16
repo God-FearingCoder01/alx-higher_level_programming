468926f413823e645ce2ff04172172f2d4f05cdc
GFC-01 <mhlopenkosikhona@gmail.com>
2023-01-16 21:54:06 +0000

    modified code

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for letter in str:
-        for i in range(97, 123):
-            if letter == chr(i):
-                print("{}".format(chr(i - 32)), end="")
-                break
-            else:
-                print(letter, end="")
-   print()
+    tmp = list(str)
+    for i in range(len(tmp)):
+        if (ord(tmp[i]) > 96 and ord(tmp[i]) < 123):
+            tmp[i] = chr(ord(tmp[i]) - 32)
+    print("{}".format(("").join(tmp)))
